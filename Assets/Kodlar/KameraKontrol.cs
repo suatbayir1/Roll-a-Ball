0f1ba7b4170383a7590a6d1b0f4dcccb6e54ce5a
Suat BAYIR <suatbayir1@gmail.com>
2019-11-04 17:30:04 +0300

    Unity 3D

---
+++ Assets/Kodlar/KameraKontrol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraKontrol : MonoBehaviour
+{
+    public GameObject top;
+    Vector3 aradakiMesafe;
+
+    void Start()
+    {
+        aradakiMesafe = transform.position - top.transform.position; // kamera ile top arasındaki mesafe
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = top.transform.position + aradakiMesafe; // kamerayı topdan mesafe kadar geride tut
+    }
+}
---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    public int hiz;
+    int sayac=0;
+    Rigidbody fizik;
+    public int toplanicakObjeSayisi;
+    public Text sayacText;
+    public Text oyunBittiText;
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");   // topu saga sola hareket
+        float dikey = Input.GetAxisRaw("Vertical");
+        
+        Vector3 vec = new Vector3(yatay, 0, dikey); // y = 0, x ve z değeri yukarıdan geliyor
+
+        fizik.AddForce(vec*hiz);
+    }
+
+    // trigger bir objeye temas edıldıgınde bu metot calısır.
+    // ontriggerenter temas oldugunda 1 kera calısır
+    // ontriggerstay temas oldugu surece calısır
+    // ontriggerexit temas olduktan cıktıktan sonra calısır.
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "engel") // gelen objenin tag'i engel ise onu yok eder
+        {
+            other.gameObject.SetActive(false);
+            sayac++; // objeye her carpıldıgında sayacı 1 arttır
+            Debug.Log(sayac);
+            //Destroy(other.gameObject); // temas edilen objeyi yok eder
+
+            sayacText.text = "Sayac : " + sayac;
+
+            if (sayac == toplanicakObjeSayisi)
+            {
+                oyunBittiText.text = "Tebrikler!! Hepsini topladınız";
+            }
+        }
+    }
+}
